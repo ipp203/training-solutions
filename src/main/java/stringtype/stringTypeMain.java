ea9311b82d0e5f16fd68af118428f19579cbd0f9
ipp203 <ipp203@freemail.hu>
2020-11-03 09:59:49 +0100

    StringType and UserValidation

---
+++ src/main/java/stringtype/stringTypeMain.java
@@ -0,0 +1,21 @@
+package stringtype;
+
+public class stringTypeMain {
+    public static void main(String[] args) {
+        String prefix = "Hello";
+        String name = "Jhon Doe";
+        String message = prefix+" "+name;
+        message = message + 444;
+        boolean b = message.equals("Hello Jhon Doe");
+        boolean c = message.equals("Hello Jhon Doe444");
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println((""+"").length());
+
+        String s = "Abcde";
+        System.out.println(s.length());
+        System.out.println(s.substring(0,1) + s.substring(2,3));
+        System.out.println(s.substring(0,3));
+
+    }
+}
