86bbe43c8de97268feddf6d01a96a9dc9d0d625a
ipp203 <ipp203@freemail.hu>
2020-11-05 10:56:56 +0100

    Gentleman, TaxCalculator, CircleCalculator, CylinderCalculator, CylinderCalculatorBasedOnCircle

---
+++ src/main/java/finalmodifier/Gentleman.java
@@ -0,0 +1,17 @@
+package finalmodifier;
+
+import java.util.Scanner;
+
+public class Gentleman {
+    private static final String MESSAGE_PREFIX = "Hello ";
+    public String sayHello(String name){
+        return MESSAGE_PREFIX+name;
+    }
+
+    public static void main(String[] args) {
+        Gentleman gentleman = new Gentleman();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Írd be a neved!");
+        System.out.println(gentleman.sayHello(scanner.nextLine()));
+    }
+}
