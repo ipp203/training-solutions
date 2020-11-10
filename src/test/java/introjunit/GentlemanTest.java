49e497df4c76b43db973243b4cdb116c9f046f4b
ipp203 <ipp203@freemail.hu>
2020-11-10 15:17:03 +0100

    Circle, MathOperations

---
+++ src/test/java/introjunit/GentlemanTest.java
@@ -0,0 +1,20 @@
+package introjunit;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class GentlemanTest {
+    @Test
+    public void testCreate(){
+        //Given
+        Gentleman gentleman = new Gentleman();
+        //When
+        String greeting = gentleman.sayHello("John Doe");
+        String greetingNull = gentleman.sayHello(null);
+        //Then
+        assertThat(greeting, equalTo("Hello John Doe"));
+        assertThat(greetingNull, equalTo("Hello Anonymus"));
+    }
+}
