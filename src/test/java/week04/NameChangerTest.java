30508661b09f51cc82fafc128bcde896bd8c7054
ipp203 <ipp203@freemail.hu>
2020-11-18 15:18:18 +0100

    BankAccount, Transactions

--- src/test/java/week04/NameChangerTest.java
+++ src/test/java/week04/NameChangerTest.java
@@ -3,9 +3,16 @@ package week04;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 public class NameChangerTest {
+    @Test
+    public void crateNameChangerTest() {
+        IllegalArgumentException iae = assertThrows(IllegalArgumentException.class, () -> new NameChanger(""));
+        assertEquals("Invalid name:", iae.getMessage());
+    }
+
     @Test
     void createTest() {
         NameChanger nameChanger = new NameChanger("Doe Jhon");
