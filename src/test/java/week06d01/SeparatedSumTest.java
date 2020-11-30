cab4789e069a66f6550b309c33c62332498e4887
ipp203 <ipp203@freemail.hu>
2020-11-30 14:25:05 +0100

    SeparatedSum

---
+++ src/test/java/week06d01/SeparatedSumTest.java
@@ -0,0 +1,15 @@
+package week06d01;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SeparatedSumTest {
+    @Test
+    public void testSeparatedSum() {
+        SeparatedSum ss = new SeparatedSum();
+        String s = "1,256;-3,14;0,002;-1,86;3,742";
+        assertTrue(Math.abs(ss.sum(s).getPositiveSum() - 5.0) < 1e-8);
+        assertTrue(Math.abs(ss.sum(s).getNegativeSum() + 5.0) < 1e-8);
+    }
+}
