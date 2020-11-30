e88a0649308f5f1691d6d166da35a1f278f05fcb
ipp203 <ipp203@freemail.hu>
2020-11-30 20:40:05 +0100

    SeparatedSum

--- src/test/java/week06d01/SeparatedSumTest.java
+++ src/test/java/week06d01/SeparatedSumTest.java
@@ -11,5 +11,8 @@ class SeparatedSumTest {
         String s = "1,256;-3,14;0,002;-1,86;3,742";
         assertTrue(Math.abs(ss.sum(s).getPositiveSum() - 5.0) < 1e-8);
         assertTrue(Math.abs(ss.sum(s).getNegativeSum() + 5.0) < 1e-8);
+        assertEquals(5.0, ss.sum(s).getPositiveSum(), 1e-8);
+        assertEquals(-5.0, ss.sum(s).getNegativeSum(), 1e-8);
+
     }
 }
