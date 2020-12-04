f40e4d397e17e0952a0f6a67c2ecc82d3d970774
ipp203 <ipp203@freemail.hu>
2020-12-04 09:53:18 +0100

    Bottle

---
+++ src/test/java/week06d05/BottleTest.java
@@ -0,0 +1,44 @@
+package week06d05;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BottleTest {
+
+    @Test
+    void of() {
+        Bottle petBottle = Bottle.of(BottleType.PET_BOTTLE);
+        Bottle glassBottle = Bottle.of(BottleType.GLASS_BOTTLE);
+
+        assertEquals(BottleType.PET_BOTTLE, petBottle.getType());
+        assertEquals(BottleType.GLASS_BOTTLE, glassBottle.getType());
+    }
+
+    @Test
+    void fill() {
+        //Given
+        Bottle bottle = Bottle.of(BottleType.PET_BOTTLE);
+        //When
+        bottle.fill(250);
+        bottle.fill(340);
+
+        //Then
+        assertEquals(590, bottle.getFilledUntil());
+    }
+
+    @Test
+    void fillMoreThanMaximum() {
+        //Given
+        Bottle bottle = Bottle.of(BottleType.GLASS_BOTTLE);
+        //When
+        bottle.fill(650);
+
+        //Then
+        assertThrows(IllegalStateException.class, () -> {
+            bottle.fill(540);
+        });
+        assertEquals(1000, bottle.getFilledUntil());
+
+    }
+}
