ce549ed889ad7bc93234cdad3d7712b9446ad27c
ipp203 <ipp203@freemail.hu>
2020-12-05 21:14:54 +0100

    C3PO, AstroBoy

---
+++ src/test/java/interfaceextends/Point.java
@@ -0,0 +1,25 @@
+package interfaceextends;
+
+import org.junit.jupiter.api.Test;
+
+public class Point {
+    private final long x, y, z;
+
+    public Point(long x, long y, long z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public long getX() {
+        return x;
+    }
+
+    public long getY() {
+        return y;
+    }
+
+    public long getZ() {
+        return z;
+    }
+}
