0178bafa95e1c61dd09bbd7b8d833e418c923ef7
ipp203 <ipp203@freemail.hu>
2020-12-06 15:32:26 +0100

    SeatHeaterState, TrafficLight, TypeWriterState

---
+++ src/test/java/statemachine/TypeWriterStateTest.java
@@ -0,0 +1,15 @@
+package statemachine;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TypeWriterStateTest {
+
+    @Test
+    public void testNext() {
+
+        assertEquals(TypeWriterState.UPPERCASE, TypeWriterState.LOWERCASE.next());
+        assertEquals(TypeWriterState.LOWERCASE, TypeWriterState.UPPERCASE.next());
+    }
+}
