65a15a7576e2432dd530bef1f96b43ef9558d95e
ipp203 <ipp203@freemail.hu>
2020-12-07 12:06:53 +0100

    Fibonacci with cache

--- src/main/java/week07d01/Fibonacci.java
+++ src/main/java/week07d01/Fibonacci.java
@@ -2,6 +2,8 @@ package week07d01;
 
 public class Fibonacci {
 
+    public static Long[] valuesOfFibonacci = new Long[101];
+
     public long fib(int n) {
         if (n < 0) {
             throw new IllegalArgumentException("n parameter must be greater, than 0");
@@ -12,6 +14,10 @@ public class Fibonacci {
         if (n == 1) {
             return 1;
         }
-        return fib(n - 2) + fib(n - 1);
+        if (valuesOfFibonacci[n] == null) {
+            valuesOfFibonacci[n] = fib(n - 2) + fib(n - 1);
+        }
+        return valuesOfFibonacci[n];
+        //return fib(n - 2) + fib(n - 1);
     }
 }
--- src/test/java/week07d01/FibonacciTest.java
+++ src/test/java/week07d01/FibonacciTest.java
@@ -6,12 +6,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FibonacciTest {
 
+
     @Test
     void testFibonacci() {
         Fibonacci fibi = new Fibonacci();
         assertEquals(2, fibi.fib(3));
         assertEquals(34, fibi.fib(9));
-
+        System.out.println(fibi.fib(100));
     }
 
 }
