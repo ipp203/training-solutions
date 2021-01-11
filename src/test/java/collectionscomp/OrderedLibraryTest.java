3a14b74eb428810b78329d419962d3049a74b3d2
ipp203 <ipp203@freemail.hu>
2021-01-11 15:08:29 +0100

    OrderedLibrary

---
+++ src/test/java/collectionscomp/OrderedLibraryTest.java
@@ -0,0 +1,61 @@
+package collectionscomp;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class OrderedLibraryTest {
+
+    private OrderedLibrary library;
+
+
+    @BeforeEach
+    public void setUp() {
+        List<Book> bookList = new LinkedList<>(Arrays.asList(new Book(2245, "Őrület", "Utasy Kristóf"),
+                new Book(1454, "Álom", "Ómolnár Géza"),
+                new Book(5454, "Államhatár", "Ákos János"),
+                new Book(5454, "Utitársak", "Ős János")));
+
+        library = new OrderedLibrary(bookList);
+    }
+
+    @Test
+    public void testOrderedByTitle() {
+        // Given
+        List<Book> orderedBookList = library.orderedByTitle();
+        //Then
+        assertEquals("Utitársak", orderedBookList.get(0).getTitle());
+    }
+
+    @Test
+    public void testOrderedByAuthor() {
+        // Given
+        List<Book> orderedBookList = library.orderedByAuthor();
+        //Then
+        assertEquals("Utasy Kristóf", orderedBookList.get(0).getAuthor());
+    }
+
+    @Test
+    public void testOrderedByTitleLocale() {
+        // Given
+        List<String> orderedTitleList = library.orderedByTitleLocale(new Locale("hu", "HU"));
+        //Then
+        assertEquals("Államhatár", orderedTitleList.get(0));
+    }
+
+    @Test
+    public void testOrderedNatural(){
+        // Given
+        List<Book> orderedBookList = library.orderedByRegNumb();
+        //Then
+        assertEquals("Ómolnár Géza", orderedBookList.get(0).getAuthor());
+
+    }
+}
