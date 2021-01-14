da23768a79352f9132b3085ca8669c5c713ebe38
ipp203 <ipp203@freemail.hu>
2021-01-14 10:14:19 +0100

    NameLength

---
+++ src/test/java/week11/d04/NameLengthTest.java
@@ -0,0 +1,33 @@
+package week11.d04;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NameLengthTest {
+
+    @Test
+    void getLengthsTest() {
+        NameLength nameLength = new NameLength();
+        List<Integer> result = nameLength.getLengths(Arrays.asList("Joe", "Jack", "Jane", "Jake", "George", "William"));
+        assertEquals(2, result.size());
+
+        assertArrayEquals(new Integer[]{3, 4}, result.toArray());
+    }
+
+    @Test
+    void getLengthsTestWithEmptyParameter() {
+        NameLength nameLength = new NameLength();
+        List<Integer> result = nameLength.getLengths(new ArrayList<>());
+        assertEquals(0, result.size());
+
+        result = nameLength.getLengths(Arrays.asList("Joe", "Jack", "Jane", "Jake", "George", ""));
+        assertEquals(2, result.size());
+
+        assertArrayEquals(new Integer[]{3, 4}, result.toArray());
+    }
+}
