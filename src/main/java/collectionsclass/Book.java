55fedbb55c5e5bc1c07a06d1b932974d8ab6cb47
ipp203 <ipp203@freemail.hu>
2021-01-17 16:15:28 +0100

    CollectionManager

---
+++ src/main/java/collectionsclass/Book.java
@@ -0,0 +1,50 @@
+package collectionsclass;
+
+import java.util.Objects;
+
+public class Book implements Comparable<Book> {
+    private int id;
+    private String author;
+    private String title;
+
+    public Book(int id, String author, String title) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    @Override
+    public int compareTo(Book o) {
+        return id - o.id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return id == book.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return id + " " + author + " " + title;
+    }
+}
