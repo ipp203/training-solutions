a4815e0cd6062fb9e77bff4f031a6fff04ddf667
ipp203 <ipp203@freemail.hu>
2021-02-11 16:14:10 +0100

    Maven test repair

--- src/main/java/controliteration/dictionary/DictionaryItem.java
+++ src/main/java/controliteration/dictionary/DictionaryItem.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class DictionaryItem {
     private String word;
-    private List<String> translations = new ArrayList();
+    private List<String> translations = new ArrayList<>();
 
     public DictionaryItem(String word, List<String> translations) {
         this.word = word;
