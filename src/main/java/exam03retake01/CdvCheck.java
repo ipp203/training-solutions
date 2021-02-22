185e7e12a0ce851c017f04b337ea804f230a9a29
ipp203 <ipp203@freemail.hu>
2021-02-22 17:51:58 +0100

    CdvCheck

--- src/main/java/exam03retake01/CdvCheck.java
+++ src/main/java/exam03retake01/CdvCheck.java
@@ -2,4 +2,35 @@ package exam03retake01;
 
 public class CdvCheck {
 
+    public static final int NUMBER_LENGTH = 10;
+    public static final int CHECK_NUMBER = 11;
+
+    public boolean check(String cvdNumber) {
+        validateNumber(cvdNumber);
+
+        char[] numberCharArray = cvdNumber.toCharArray();
+        int[] numbers = new int[NUMBER_LENGTH];
+
+        for (int i = 0; i < NUMBER_LENGTH; i++) {
+            numbers[i] = Character.getNumericValue(numberCharArray[i]);
+        }
+
+        int result = 0;
+        for (int i = 0; i < NUMBER_LENGTH - 1; i++) {
+            result += (i + 1) * numbers[i];
+        }
+        return result % CHECK_NUMBER == numbers[NUMBER_LENGTH - 1];
+    }
+
+    private void validateNumber(String cvdNumber) {
+        if (cvdNumber.length() != NUMBER_LENGTH) {
+            throw new IllegalArgumentException("Arguments length must be 10!");
+        }
+        char[] numberCharArray = cvdNumber.toCharArray();
+        for (char c : numberCharArray) {
+            if (Character.isAlphabetic(c)) {
+                throw new IllegalArgumentException("Argument must be number");
+            }
+        }
+    }
 }
--- src/test/java/exam03retake01/CdvCheckTest.java
+++ src/test/java/exam03retake01/CdvCheckTest.java
@@ -6,18 +6,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CdvCheckTest {
 
-//    @Test
-//    void check() {
-//        assertTrue(new CdvCheck().check("8365670003"));
-//        assertFalse(new CdvCheck().check("8365670004"));
-//    }
-//
-//    @Test
-//    void notNumber() {
-//        assertThrows(IllegalArgumentException.class,
-//                () -> new CdvCheck().check("123456789"));
-//
-//        assertThrows(IllegalArgumentException.class,
-//                () -> new CdvCheck().check("12345a7890"));
-//    }
+    @Test
+    void check() {
+        assertTrue(new CdvCheck().check("8365670003"));
+        assertFalse(new CdvCheck().check("8365670004"));
+    }
+
+    @Test
+    void notNumber() {
+        assertThrows(IllegalArgumentException.class,
+                () -> new CdvCheck().check("123456789"));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> new CdvCheck().check("12345a7890"));
+    }
 }
